13a7cab9d41024cf847905cc22ffaa5330b28837
Hyun Joo <37135241+worlddown@users.noreply.github.com>
2018-06-25 19:05:07 +0900

    18.6.25 JH homework

---
+++ RestFullApi/src/JH_factoryTest/Benz.java
@@ -0,0 +1,17 @@
+package factoryTest;
+
+public class Benz extends Vehicle {
+
+	public Benz() 
+	{
+		
+	}
+
+	@Override
+	public void lease() 
+	{
+		System.out.println("벤츠를 사세요");
+
+	}
+
+}
---
+++ RestFullApi/src/JH_factoryTest/Vehicle.java
@@ -0,0 +1,6 @@
+package factoryTest;
+
+public abstract class Vehicle 
+{
+	public abstract void lease();
+}
